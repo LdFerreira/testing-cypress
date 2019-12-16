b3499644c6b55b9d63f475c933b889a4390b5b82
Leandro <leandro.ferreira@nexxera.com>
2019-12-16 19:43:54 -0300

    Aula hooks

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,14 +1,26 @@
 /// <reference types="cypress"/>
 
 describe("Work with basic element", () => {
-  it("Text", () => {
+  beforeEach(() => {
     cy.visit("https://wcaquino.me/cypress/componentes.html");
+  });
 
+  it("Text", () => {
     //cy.get("body").should("have.text", "Cuidado");
     cy.get("span").should("contain", "Cuidado");
     cy.get(".facilAchar").should(
       "have.text",
       "Cuidado onde clica, muitas armadilhas..."
     );
+  });
+
+  it("Links", () => {
+    cy.contains("Voltar").click();
+    cy.get("#resultado").should("have.text", "Voltou!");
+
+    cy.reload();
+    cy.get("#resultado").should("have.not.text", "Voltou!");
+    cy.contains("Voltar").click();
+    cy.get("#resultado").should("have.text", "Voltou!");
   });
 });
