588c53f129a50f84bc417c10ab1f4a9e78c3488a
Leandro <leandro.ferreira@nexxera.com>
2020-01-14 08:46:12 -0300

    Fim da sessao 5

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -10,10 +10,22 @@ describe("Cypress basics", () => {
     cy.title()
       .should("be.equal", "Campo de Treinamento")
       .and("contain", "Campo");
-
+    let syncTitle
     cy.title().then(title => {
       console.log(title);
+
+      cy.get('#formNome').type(title)
+
+      syncTitle = title
     });
+
+    cy.get('[data-cy=dataSobrenome]').then($el =>{
+      $el.val(syncTitle) 
+    })
+
+    cy.get('#elementosForm\\:sugestoes').then($el => {
+      cy.wrap($el).type(syncTitle)
+    })
   });
 
   it("Should find and interact with an element", () => {
